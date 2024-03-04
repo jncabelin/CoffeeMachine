7703bd9da0aa51830ddaeef71083cf8fb10dbfeb
jncabelin <diethercabelin@gmail.com>
2024-03-04 23:46:20 +0800

    added unit tests for dto

---
+++ CoffeeMachineSystem.Tests/CoffeeMachine.Api.UnitTests/Dtos/JsonConvert_should.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
+using CoffeeMachine.Tests.Api.UnitTests.TestHelpers;
+using CoffeeMachine.Api.Dtos.Responses;
+using CoffeeMachine.Api.Messages;
+
+namespace CoffeeMachine.Tests.Api.UnitTests.Dtos
+{
+    public class JsonConvert_should
+    {
+        [Fact]
+        [DisplayName("Serialize and Deserialize Clouds")]
+        public void Serialize_and_Deserialize_Clouds()
+        {
+            // Arrange
+            var response = new BrewCoffeeResponse(statusMessage: ResponseMessage.OK, DateTimeOffset.UtcNow);
+
+            // Act
+            var jsonResult = JsonConvert.SerializeObject(response);
+            var objResult = JsonConvert.DeserializeObject<BrewCoffeeResponse>(jsonResult);
+
+            // Assert
+            Assert.NotNull(jsonResult);
+            Assert.NotNull(objResult);
+            Assert.Contains($"\"message\":", jsonResult);
+            Assert.Contains($"\"prepared\":", jsonResult);
+            Assert.True(HelperMethods.Compare<BrewCoffeeResponse>(response, objResult));
+
+        }
+    }
+}
+
