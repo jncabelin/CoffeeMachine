280ec57fb834863522b9d6f4986181fc919ff972
jncabelin <diethercabelin@gmail.com>
2024-03-05 00:09:23 +0800

    added openweathermap classes

---
+++ CoffeeMachine/Models/OpenWeatherMap/CurrentWeather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CoffeeMachine.Api.Models.OpenWeatherMap
+{
+	public class CurrentWeather
+    {
+        [JsonProperty("coord")]
+        public Coordinates Coordinates { get; set; }
+
+        [JsonProperty("weather")]
+        public List<Weather> Weather { get; set; } = new List<Weather>();
+
+        [JsonProperty("base")]
+        public string Base { get; set; }
+
+        [JsonProperty("main")]
+        public MainWeatherData MainWeatherData { get; set; }
+
+        [JsonProperty("visibility")]
+        public double Visibility { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonProperty("dt")]
+        public int DT { get; set; }
+
+        [JsonProperty("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonProperty("timezone")]
+        public int Timezone { get; set; }
+
+        [JsonProperty("id")]
+        public int ID { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("cod")]
+        public int Cod { get; set; }
+    }
+}
+
---
+++ CoffeeMachine/Models/OpenWeatherMap/Wind.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace CoffeeMachine.Api.Models.OpenWeatherMap
+{
+	public class Wind
+	{
+        [JsonProperty("speed")]
+        public Double Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public Double Degree { get; set; }
+    }
+}
+
