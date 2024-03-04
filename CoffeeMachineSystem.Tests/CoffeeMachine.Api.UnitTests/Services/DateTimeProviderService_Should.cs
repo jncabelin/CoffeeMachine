3a01476308c9388bc6d20bd4d21736a01226903d
jncabelin <diethercabelin@gmail.com>
2024-03-04 23:46:35 +0800

    added unit tests for services

---
+++ CoffeeMachineSystem.Tests/CoffeeMachine.Api.UnitTests/Services/DateTimeProviderService_Should.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using CoffeeMachine.Api.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CoffeeMachine.Tests.Api.UnitTests.Services
+{
+	public class DateTimeProviderService_Should
+	{
+        Mock<ILogger<DateTimeProviderService>> _logger;
+        public DateTimeProviderService_Should()
+		{
+			_logger = new Mock<ILogger<DateTimeProviderService>>();
+		}
+
+        [Fact]
+        [DisplayName("Throw Exception if Null ILogger")]
+        public void Throw_Exception_if_Null_ILogger()
+        {
+			// Arrange
+			Action testConstructor = () => new DateTimeProviderService(null);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(testConstructor);
+        }
+		
+        [Fact]
+		[DisplayName("Verify if AprilFirst")]
+		public void Verify_if_AprilFirst()
+		{
+			// Arrange
+			var service = new DateTimeProviderService(_logger.Object);
+
+			// Act
+			var result = service.IsAprilFirst();
+
+			// Assert
+			Assert.True(result.IsSuccess);
+			if (DateTime.Now.Day == 1 && DateTime.Now.Month == 4)
+				Assert.True(result.Value);
+			else
+				Assert.False(result.Value);
+		}
+	}
+}
+
---
+++ CoffeeMachineSystem.Tests/CoffeeMachine.Api.UnitTests/Services/FakeCoffeeMachineService_Should.cs
@@ -0,0 +1,61 @@
+﻿using CoffeeMachine.Api.Services;
+using System.ComponentModel;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CoffeeMachine.Tests.Api.UnitTests.Services
+{
+	public class FakeCoffeeMachineService_Should
+	{
+        Mock<ILogger<FakeCoffeeMachineService>> _logger;
+
+        public FakeCoffeeMachineService_Should()
+        {
+            _logger = new Mock<ILogger<FakeCoffeeMachineService>>();
+        }
+
+        [Fact]
+        [DisplayName("Throw Exception if Null ILogger")]
+        public void Throw_Exception_if_Null_ILogger()
+        {
+            // Arrange
+            Action testConstructor = () => new FakeCoffeeMachineService(null);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(testConstructor);
+        }
+
+        [Fact]
+        [DisplayName("BrewProduct_Once")]
+        public async void BrewProduct_Once()
+        {
+            // Arrange
+            var service = new FakeCoffeeMachineService(_logger.Object);
+
+            // Act
+            var result = await service.BrewProduct();
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(1, result.Value);
+        }
+
+        [Fact]
+        [DisplayName("Update_ProductCounter")]
+        public async void Update_ProductCounter()
+        {
+            // Arrange
+            var service = new FakeCoffeeMachineService(_logger.Object);
+
+            // Act and Assert
+            for (var i = 1; i <= 5; i++)
+            {
+                var result = await service.BrewProduct();
+
+                Assert.True(result.IsSuccess);
+                Assert.Equal(i, result.Value);
+            }
+        }
+    }
+}
+
